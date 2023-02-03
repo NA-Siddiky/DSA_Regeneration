e5de86997ac11302a59477696db4d64ef2f7177d
NA-Siddiky <siddiky.academic@gmail.com>
2023-02-03 23:53:59 +0600

    adding a issue of mac run

---
+++ Problems/Outsbook/p2.cpp
@@ -0,0 +1,21 @@
+// #include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <sstream>
+// #include <bits/stdc++.h>
+#include <iomanip> // std::setprecision
+#include <math.h>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    vector<int> a = {1, 2, 3, 4, 5};
+    // a.resize(3);
+    for (int i = 0; i < (int)a.size(); i++)
+    {
+        cout << a[i] << endl;
+    }
+    return 0;
+}
